808c40adc0da02d8578ec5176f9e810d5a9bf82a
Muhammadyusuf96 <36552832+Muhammadyusuf96@users.noreply.github.com>
2021-07-02 13:44:18 +0500

    some changes

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # passport_scan
 Biometrik passportdan malumotlarni o'qib olish dasturi
 
-Passport malumotlari Internetdan olidni!
+Passport malumotlari Internetdan olindi!
 
 # Ubuntu 20.04
 ````
@@ -22,3 +22,12 @@ sudo apt install libtesseract-dev
 ````
 tesseract --version
 ````
+
+# Windows
+
+1. Install tesseract using windows installer available [here](https://github.com/UB-Mannheim/tesseract/wiki).
+2. Note the tesseract path from the installation. Default installation path at the time of this edit was: `C:\Program Files\Tesseract-OCR\`. It may change so please check the installation path.
+**WARNING: Tesseract should be either installed in the directory which is suggested during the installation or in a new directory. The uninstaller removes the whole installation directory. If you installed Tesseract in an existing directory, that directory will be removed with all its subdirectories and files.**
+4. In cmd: `pip install -r requirements.txt`
+5. Set the tesseract path in the script before calling  `image_to_string`:
+`pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'`
--- main.py
+++ main.py
@@ -3,70 +3,101 @@ import pytesseract
 
 import numpy as np
 import math
+import sys
+
+if sys.platform.startswith('win'):
+    # Windows-specific code here...
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 
 #####################################
 
-img = cv2.imread('a.jpg',0)
+img = cv2.imread('images/ale.jpg', 0)
 (height, width) = img.shape
 
 #####################################
 
 img_copy = img.copy()
 
-img_canny = cv2.Canny(img_copy, 50, 100, apertureSize = 3)
+img_canny = cv2.Canny(img_copy,
+                      50,
+                      100,
+                      apertureSize=3
+                      )
 
 
-img_hough = cv2.HoughLinesP(img_canny, 1, math.pi / 180, 100, minLineLength = 100, maxLineGap = 10)
+img_hough = cv2.HoughLinesP(img_canny,
+                            1,
+                            math.pi / 180,
+                            100,
+                            minLineLength=100,
+                            maxLineGap=10
+                            )
 
 
-(x, y, w, h) = (np.amin(img_hough, axis = 0)[0,0], np.amin(img_hough, axis = 0)[0,1],
-np.amax(img_hough, axis = 0)[0,0] - np.amin(img_hough, axis = 0)[0,0],
-np.amax(img_hough, axis = 0)[0,1] - np.amin(img_hough, axis = 0)[0,1])
+(x, y, w, h) = (np.amin(img_hough, axis=0)[0, 0],
+                np.amin(img_hough, axis=0)[0, 1],
+                np.amax(img_hough, axis=0)[0, 0] - np.amin(img_hough, axis=0)[0, 0],
+                np.amax(img_hough, axis=0)[0, 1] - np.amin(img_hough, axis=0)[0, 1]
+                )
 
-img_roi = img_copy[y:y+h,x:x+w]
+img_roi = img_copy[y:y + h, x:x + w]
 
 #####################################
 
 (height, width) = img_roi.shape
 
 img_roi_copy = img_roi.copy()
-dim_mrz = (x, y, w, h) = (1, round((height*0.9)+1), width-3, round(height-(height*0.9))-2)
+dim_mrz = (x, y, w, h) = (1,
+                          round((height * 0.9) + 1),
+                          width - 3,
+                          round(height - (height * 0.9)) - 2
+                          )
 
-img_roi_copy = cv2.rectangle(img_roi_copy, (x, y-30), (x + w ,y + h),(0,0,0),2)
+img_roi_copy = cv2.rectangle(img_roi_copy,
+                             (x, y - 30),
+                             (x + w, y + h),
+                             (0, 0, 0),
+                             2
+                             )
 
-img_mrz = img_roi[y-36:y+h-13, x:x+w]
+img_mrz = img_roi[y - 36:y + h - 13, x:x + w]
 
 
 img_mrz = cv2.cvtColor(img_mrz, cv2.COLOR_BGR2RGB)
-ret, img_mrz = cv2.threshold(img_mrz,127,255,cv2.THRESH_TOZERO)
+ret, img_mrz = cv2.threshold(img_mrz,
+                             127,
+                             255,
+                             cv2.THRESH_TOZERO
+                             )
 
-mrz = pytesseract.image_to_string(img_mrz, config = r'--oem 3 --psm 6')
+mrz = pytesseract.image_to_string(img_mrz, config=r'--oem 3 --psm 6')
+
+print(mrz)
 
 mrz = [line for line in mrz.split('\n')]
 if mrz[0][0:2] == 'P<':
     lastname = mrz[0].split('<')[1][3:]
-    print("lastname",lastname)
+    print("lastname", lastname)
 else:
     lastname = mrz[0].split('<')[0][5:]
-    print("lastname",lastname)
+    print("lastname", lastname)
 firstname = [i for i in mrz[0].split('<<') if (i).isspace() == 0 and len(i) > 0 and i != "<"][1].partition('<')[0]
-print("firstname",firstname)
-print("mrz",mrz)
+print("firstname", firstname)
+print("mrz", mrz)
 pp_no = mrz[1][:9]
-print("pp_no",pp_no)
+print("pp_no", pp_no)
 pinfl = mrz[1][28:42]
-print("pinfl",pinfl)
+print("pinfl", pinfl)
 ###################################
 
 img_roi_copy = img_roi.copy()
 dim_lastname_chi = (x, y, w, h) = (455, 1210, 120, 70)
-img_roi_copy = cv2.rectangle(img_roi_copy, (x, y), (x + w ,y + h),(0,0,0))
+img_roi_copy = cv2.rectangle(img_roi_copy, (x, y), (x + w, y + h), (0, 0, 0))
 
 
 passport_dict = {'Passport No.': pp_no,
                  'First Name': firstname,
                  'Last Name': lastname,
-                 'Pinfl':pinfl,
-                }
+                 'Pinfl': pinfl}
 print(passport_dict)
